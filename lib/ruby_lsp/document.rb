a76f9ab6eead6a51b8652d4625cca8aba154b01e
Vinicius Stock <vinicius.stock@shopify.com>
2024-07-26 09:44:15 -0400

    Add the ability to locate the first node within a range

--- lib/ruby_lsp/document.rb
+++ lib/ruby_lsp/document.rb
@@ -223,6 +223,48 @@ module RubyLsp
       NodeContext.new(closest, parent, nesting_nodes, call_node)
     end
 
+    sig do
+      params(
+        range: T::Hash[Symbol, T.untyped],
+        node_types: T::Array[T.class_of(Prism::Node)],
+      ).returns(T.nilable(Prism::Node))
+    end
+    def locate_first_within_range(range, node_types: [])
+      scanner = create_scanner
+      start_position = scanner.find_char_position(range[:start])
+      end_position = scanner.find_char_position(range[:end])
+      desired_range = (start_position...end_position)
+      queue = T.let(@parse_result.value.child_nodes.compact, T::Array[T.nilable(Prism::Node)])
+      closest = T.let(nil, T.nilable(Prism::Node))
+
+      until queue.empty? || closest
+        candidate = queue.shift
+
+        # Skip nil child nodes
+        next if candidate.nil?
+
+        # Add the next child_nodes to the queue to be processed. The order here is important! We want to move in the
+        # same order as the visiting mechanism, which means searching the child nodes before moving on to the next
+        # sibling
+        T.unsafe(queue).unshift(*candidate.child_nodes)
+
+        # Skip if the current node doesn't cover the desired position
+        loc = candidate.location
+        next unless desired_range.cover?(loc.start_offset...loc.end_offset)
+
+        # If the node's start character is already past the position, then we should've found the closest node
+        # already
+        break if end_position < loc.start_offset
+
+        # If there are node types to filter by, and the current node is not one of those types, then skip it
+        next if node_types.any? && node_types.none? { |type| candidate.class == type }
+
+        closest = candidate
+      end
+
+      closest
+    end
+
     sig { returns(SorbetLevel) }
     def sorbet_level
       sigil = parse_result.magic_comments.find do |comment|
